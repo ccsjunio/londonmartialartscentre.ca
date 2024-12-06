9637b4cf50f855ef246d27d0449d50063b309e5d
Carlos Cesar da Silva Ferraz Junior <carlos.cesar.ferraz@gmail.com>
2024-12-06 15:48:36 -0500

    Upload icons for social media and implement on footer

--- footer.php
+++ footer.php
@@ -3,13 +3,13 @@
         <p>&copy; 2024 London Martial Arts Centre. All rights reserved.</p>
         <div class="social-links">
             <a href="https://facebook.com/londonmartialartscentre" target="_blank" aria-label="Facebook">
-                <img src="images/facebook-icon.png" alt="Facebook">
+                <img src="images/facebook-icon.webp" alt="Facebook">
             </a>
             <a href="https://instagram.com/londonmartialartscentre" target="_blank" aria-label="Instagram">
-                <img src="images/instagram-icon.png" alt="Instagram">
+                <img src="images/instagram-icon.webp" alt="Instagram">
             </a>
             <a href="mailto:mail@londonmartialartscentre.ca" aria-label="Email">
-                <img src="images/email-icon.png" alt="Email">
+                <img src="images/email-icon.webp" alt="Email">
             </a>
         </div>
     </div>
