5e1c32d8d5908a187386c295ee4fe8743510f7c4
Carlos Cesar da Silva Ferraz Junior <carlos.cesar.ferraz@gmail.com>
2024-12-08 18:17:41 -0500

    Implement the Zapier Bot in the page

--- footer.php
+++ footer.php
@@ -21,5 +21,7 @@
         </div>
     </div>
 </footer>
+<script async type='module' src='https://interfaces.zapier.com/assets/web-components/zapier-interfaces/zapier-interfaces.esm.js'></script>
+<zapier-interfaces-chatbot-embed is-popup='true' chatbot-id='clycimeyr000bt1bjx3d6hyhb'></zapier-interfaces-chatbot-embed>
 </body>
 </html>
