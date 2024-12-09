ee611867ee1de7997d7131d8fb6d55d749a47ff5
Carlos Cesar da Silva Ferraz Junior <carlos.cesar.ferraz@gmail.com>
2024-12-08 21:01:44 -0500

    Included the contact and the form on the new service pages

--- birthday-parties.php
+++ birthday-parties.php
@@ -11,6 +11,15 @@ include 'header.php';
             <p>Whether you are looking to improve your fitness, learn self-defense, or explore mindfulness practices through Tai Chi, our group classes cater to all levels and goals.</p>
         </div>
     </section>
+    <section id="contact" class="contact">
+		<div class="container">
+			<h2>Contact Us</h2>
+			<p>Please let us know that you are interested on these services! Leave your contact information below and we will contact you soon!</p>
+
+			<?php include 'form-container.php'; ?>
+
+		</div>
+	</section>
 </main>
 
 <?php include 'footer.php'; ?>
