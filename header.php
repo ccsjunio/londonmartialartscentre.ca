993720b60ff6042219431a5f77a691b002084614
ccsjunio <carlos.cesar.ferraz@gmail.com>
2025-01-20 11:03:50 -0700

    adjust gtm settings

--- header.php
+++ header.php
@@ -22,6 +22,15 @@ $currentDomain = $protocol . "://" . $domain;
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
     <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
     <script src="https://gymdesk.com/js/widgets.js"></script>
+    
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-N3L3TNZR');</script>
+    <!-- End Google Tag Manager -->
+    
 </head>
 <body>
     <!-- Google Tag Manager (noscript) -->
